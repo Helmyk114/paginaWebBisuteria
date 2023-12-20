3dd644cde0d369a55d369b0b962eee89bf653f23
sarac721 <123259230+sarac721@users.noreply.github.com>
2023-12-20 17:46:46 -0500

    Se creo la carpeta pages y bienvenida y error

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,8 @@
 import './App.css';
-import Footer from './components/Footer/Footer';
-import Navegar from './components/Navbar/Navbar';
-
 
 function App() {
   return (
     <div>
-
-      <Navegar/>
-       
-      <Footer/>
-     
       
        
 
---
+++ client/src/pages/error.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Error() {
+  return (
+    <div style={{ textAlign: 'center', marginTop: '100px' }}>
+        <h1>Error 404 </h1>
+    </div>
+  )
+}
+
+export default Error
