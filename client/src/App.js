66a5fb33f223c8d8b744a37d7e53ef6125fe0267
sarac721 <123259230+sarac721@users.noreply.github.com>
2023-12-20 17:28:13 -0500

    Se creo el footer

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,18 @@
 import './App.css';
+import Footer from './components/Footer/Footer';
+import Navegar from './components/Navbar/Navbar';
 
 
 function App() {
   return (
     <div>
-      
 
+      <Navegar/>
+       
+      <Footer/>
+     
+      
+       
 
     </div>
   );
