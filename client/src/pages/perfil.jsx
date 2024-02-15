16fd4f1f20049345a0e2669061ae8da35b1217da
sarac721 <123259230+sarac721@users.noreply.github.com>
2024-02-14 22:49:11 -0500

    se modifico cardInfo

--- client/src/pages/perfil.jsx
+++ client/src/pages/perfil.jsx
@@ -1,26 +1,25 @@
 import React from "react";
-import {Card, CardBody} from "@nextui-org/react";
+import {Card, Image} from "@nextui-org/react";
 import CardPerfil from "../components/UI/perfil/cardInfo";
 
 
 export default function Perfilinfo() {
   return (
-    <Card style={{width:"80%"}}>
+    <Card style={{width:"90%"}}>
       
       <CardPerfil
       texto={"Usuario1"}
-      icono={<svg
-        xmlns="http://www.w3.org/2000/svg"
-        width="76"
-        height="76"
-        fill="currentColor"
-        className="bi bi-person-fill"
-        viewBox="0 0 16 16"
-    >
-        <path d="M3 14s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1zm5-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6" />
-    </svg>}>
+      
+      >
 
       </CardPerfil>
+      <div style={{width:"100%", margin:"0 auto", display:"flex", justifyContent:"center", opacity:"100"}}>
+      <Image
+      src= "https://avatars.githubusercontent.com/u/86160567?s=200&v=4"
+      />
+        
+      </div>
+
        
       
     </Card>
