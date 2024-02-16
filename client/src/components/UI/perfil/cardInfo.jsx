b88a381b869163ed0aab29400ed003b1d914dc07
sarac721 <123259230+sarac721@users.noreply.github.com>
2024-02-15 21:57:29 -0500

    Se instalo libreria para iconos y se agregaron a la pagina perfil

--- client/src/components/UI/perfil/cardInfo.jsx
+++ client/src/components/UI/perfil/cardInfo.jsx
@@ -1,24 +1,20 @@
 import React from "react";
+import { Icon } from '@iconify/react';
 import {Card, CardHeader, Image} from "@nextui-org/react";
 
 
 
-export default function CardPerfil({icono, texto}) {
+export default function CardPerfil({icon, texto, texto2}) {
  
 
   return (
-    <Card className="w-[300px]">
-      <CardHeader className="justify-between">
-        <div className="flex gap-5">
-          <Image
-          height={40}
-          radius="sm"
-          width={40}
-          className="rounded-full w-10 h-11 bg-none"
-          src="https://avatars.githubusercontent.com/u/86160567?s=200&v=4"/>
-          <div className="flex flex-col gap-1 items-start justify-center">
+    <Card style={{width:"90%", margin:"0 auto"}}>
+      <CardHeader className="">
+        <div className="flex gap-3"style={{alignItems:"start"}}>
+          <Icon icon={icon} width="32" height="32" style={{color: " #6977e4"}} />
+          <div className="flex flex-col items-start justify-center">
             <h4 className="text-small font-semibold leading-none text-default-600">{texto}</h4>
-           
+            <h5 className="text-small tracking-tight text-default-400">{texto2}</h5>
           </div>
         </div>
       
