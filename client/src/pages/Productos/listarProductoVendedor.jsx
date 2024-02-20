bd20e5eda1aefb3d4a3eaa78e97072f7e726f005
JuanDa241 <juandavidgallegomunoz24@gmail.com>
2024-02-19 21:16:30 -0500

    se hizo un espaciado.

--- client/src/pages/Productos/listarProductoVendedor.jsx
+++ client/src/pages/Productos/listarProductoVendedor.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { Card, CardBody, CardFooter, Image } from "@nextui-org/react";
 import img from "../../img/collar.jpg";
-
+import { Spacer } from "@nextui-org/react";
 import { Icon } from "@iconify/react";
 import CardProduct from "../../components/UI/cardProduct/card";
 import Categorias from "../../components/UI/cardProduct/categorias";
@@ -17,8 +17,8 @@ export default function ListProduct() {
 				<Titulo espacio="center" titulo="Bienvenido"/>
 				<Notificacion/>
 			</Navigate>
-      <Categorias
-      />
+      <Spacer y={5}/>
+      <Categorias/>
       <CardProduct img={img} producto="collar de oro" precio="3000" />
       <CardProduct img={img} producto="collar de oro" precio="3000" />
       <Footer/>
