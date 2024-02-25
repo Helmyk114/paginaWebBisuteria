823156175a2856b4c31d8e6aed76cd77c432c43a
Sergio Esteban <sergio.calvo@udea.edu.co>
2024-02-25 15:05:03 -0500

    correcion de errores en listar productos

--- client/src/pages/Productos/listarProducto.jsx
+++ client/src/pages/Productos/listarProducto.jsx
@@ -1,34 +1,27 @@
 import React, { useState, useEffect } from "react";
-import CustomCard from '../../components//Card/card';
-import { CardContent } from '../../components/Card/card';
 import Flotante from '../../components/Botones/BotonFlotante/Flotante';
 import { eliminarInformacionApi, listarInformacionApi } from "../../api/productos";
 import Swal from "sweetalert2";
 
 import { Spacer, Tooltip } from "@nextui-org/react";
 import Navigate, { Notificacion, Retroceder, Titulo } from "../../components/UI/navbar/navbar";
+import CardPerfil, { Texto1Card, Texto2Card } from "../../components/UI/perfil/cardInfo"
 import Avatares from "../../components/UI/avatar/Avatares";
 import Loader from "../../components/UI/cargando/loader";
-import BotonEditar from "../../components/UI/botones/botonEditar";
-import BotonEliminar from "../../components/UI/botones/botonEliminar";
-import Footer from "../../components/UI/Footer/Footer";
-import CardPerfil, {Texto1Card, Texto2Card} from "../../components/UI/perfil/cardInfo"
 import EditIcon from "../../components/UI/iconos/Editar";
 import DeleteIcon from "../../components/UI/iconos/Eliminar";
-
-
+import Footer from "../../components/UI/Footer/Footer";
 
 function ListarProducto() {
 
 	const [informacion, setInformacion] = useState([]);
 	const [cargando, setCargando] = useState(true);
-	const endPoint = 'productos';
 	const urlImage = process.env.REACT_APP_API_URL;
 
 	useEffect(() => {
 		const data = async () => {
 			try {
-				const informacionProducto = await listarInformacionApi(endPoint);
+				const informacionProducto = await listarInformacionApi('productos');
 				setInformacion(informacionProducto.data);
 				setCargando(false);
 			} catch (error) {
@@ -50,7 +43,7 @@ function ListarProducto() {
 				confirmButtonText: "Eliminar"
 			});
 			if (result.isConfirmed) {
-				await eliminarInformacionApi(endPoint, idProduct)
+				await eliminarInformacionApi('productos', idProduct)
 				const nuevaInformacion = informacion.filter((datos) => datos.idProduct !== idProduct);
 				setInformacion(nuevaInformacion)
 
@@ -90,7 +83,6 @@ function ListarProducto() {
 				<Titulo espacio="center" titulo="Productos" />
 				<Notificacion />
 			</Navigate>
-
 			<Spacer y={5} />
 
 			{cargando ? (
@@ -99,50 +91,50 @@ function ListarProducto() {
 				<div>
 					{informacion && informacion.length > 0 ? (
 						informacion.map((datos) => (
-							<>
-							<CardPerfil 
-							justifyContent={"space-between"}
-							className="cardPerfil"
-							alignItems={"center"}
-							key={datos.idProduct}>
-								<Avatares
-									src={`${urlImage}/${datos.image}`}
-									radio={"full"}/>
-                              <div style={{ display: "flex", gap:"13px" }}>
-								<Texto1Card
-                               texto={datos.nameProduct} />
-                             </div>
-							 <div>
-							 <Texto2Card
-                                texto2={`PC: ${datos.price} $`} 
-								/>
-                             <Texto2Card
-                             texto2={`PO ${datos.laborPrice} $`} />
-							 </div>
-							 
-				<div className="relative flex items-center gap-1" style={{justifyContent:"center"}}>
-                  <Tooltip content="Editar producto">
-                    <span className="text-lg text-default-400 cursor-pointer active:opacity-50">
-                      <EditIcon ruta={`/editar/producto/${datos.idProduct}`} />
-                    </span>
-                  </Tooltip>
-                  <Tooltip content="Eliminar producto">
-                    <span className="text-lg text-danger cursor-pointer active:opacity-50">
-                      <DeleteIcon funcEliminar={() => eliminarProducto(datos.idProduct)} />
-                    </span>
-                  </Tooltip>
-                </div>  
-			</CardPerfil>
-			<Spacer y={3}/> 
-			</> 
+							<div key={datos.idProduct}>
+								<CardPerfil
+									justifyContent={"space-between"}
+									className="cardPerfil"
+									alignItems={"center"}
+									key={datos.idProduct}>
+									<Avatares
+										src={`${urlImage}/${datos.image}`}
+										radio={"full"} />
+									<div style={{ display: "flex", gap: "13px" }}>
+										<Texto1Card
+											texto={datos.nameProduct} />
+									</div>
+									<div>
+										<Texto2Card
+											texto2={`PC ${datos.price} $`}
+										/>
+										<Texto2Card
+											texto2={`PO ${datos.laborPrice} $`} />
+									</div>
+
+									<div className="relative flex items-center gap-1" style={{ justifyContent: "center" }}>
+										<Tooltip content="Editar producto">
+											<span className="text-lg text-default-400 cursor-pointer active:opacity-50">
+												<EditIcon ruta={`/editar/producto/${datos.idProduct}`} />
+											</span>
+										</Tooltip>
+										<Tooltip content="Eliminar producto">
+											<span className="text-lg text-danger cursor-pointer active:opacity-50">
+												<DeleteIcon eliminar={() => eliminarProducto(datos.idProduct)} />
+											</span>
+										</Tooltip>
+									</div>
+								</CardPerfil>
+								<Spacer y={3} />
+							</div>
 						))
 					) : (
 						<p>No hay productos disponibles.</p>
 					)}
 				</div>
 			)}
-			
-             
+
+
 			<Footer />
 		</div>
 	);
