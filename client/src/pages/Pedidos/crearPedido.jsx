15ae0e0fa6c5d547787612b164cbc9c51f6fc2f4
Sergio Esteban <sergio.calvo@udea.edu.co>
2024-02-26 16:20:21 -0500

    Se organizo crearPedido

--- client/src/pages/Pedidos/crearPedido.jsx
+++ client/src/pages/Pedidos/crearPedido.jsx
@@ -1,18 +1,22 @@
 import React, { useRef } from "react";
 import { useForm } from "react-hook-form";
+import { useLocation } from 'react-router-dom';
+import './crearPedido.css';
+
 import Navigate, { Notificacion, Retroceder, Titulo } from "../../components/UI/navbar/navbar";
-import { Card, CardHeader, CardBody, Divider, Spacer } from "@nextui-org/react";
+import Acordeon from "../../components/UI/Acordeon/Acordeon";
+import { Card, CardBody, Spacer } from "@nextui-org/react";
 import InputText from "../../components/UI/formulario/Inputs/inputText";
 import BotonEnviar from "../../components/UI/botones/botonEnviar"
 import Footer from "../../components/UI/Footer/Footer";
-import { useLocation } from 'react-router-dom';
-import './crearPedido.css';
-import Acordeon from "../../components/UI/Acordeon/Acordeon";
-
+import { añadirInformacionAPI } from "../../api/productos";
+import Swal from "sweetalert2";
 
 function CrearPedido() {
 
 	const { register, handleSubmit, formState: { errors } } = useForm();
+	const mensaje = 'Este campo es requerido'
+
 	const refs = useRef({
 		idCardClient: null,
 		nameclient: null,
@@ -21,8 +25,31 @@ function CrearPedido() {
 	});
 
 	const location = useLocation();
-  	const { state } = location;
-	  console.log(state);
+	const { state } = location;
+
+	const onSubmit = async (data) => {
+		const cliente = {
+			...data,
+		};
+		console.log(cliente)
+		try {
+			await añadirInformacionAPI(cliente, 'cliente');
+			Swal.fire({
+				icon: "success",
+				title: "Se ha creado un trabajador!",
+				showConfirmButton: false,
+				timer: 1500
+			});
+		} catch (error) {
+			console.error('Error al crear un cliente', error)
+			Swal.fire({
+				icon: "error",
+				title: "No Se puede crear un trabajador!",
+				showConfirmButton: false,
+				timer: 1500
+			});
+		}
+	};
 
 	return (
 		<div>
@@ -33,91 +60,87 @@ function CrearPedido() {
 			</Navigate>
 
 			<Spacer y={4} />
-		
-					<Acordeon titulo={'Datos del cliente'}>
 
+			<form onSubmit={handleSubmit(onSubmit)}>
+				<Acordeon titulo={'Datos del cliente'}>
 					<div className="gap-4" style={{ display: "grid", gridTemplateColumns: "2fr 2fr" }}>
-						<InputText ref={(el) => { refs.current.idCardClient = el; }}
-							{...register("idCardClient", { required: { value: true, message: 'El nombre del producto es requerido' } })}
-							key="idCardClient"
-							type="text"
-							label={<span className="custom-label">Cédula</span>}
-							labelPlacement="outside"
-							placeholder={"Escriba el nombre del producto"}
-							size="md"
-						/>
-						{errors.idCardClient && <span>{errors.idCardClient.message}</span>}
-
-						<InputText ref={(el) => { refs.current.nameclient = el; }}
-							{...register("nameclient", { required: { value: true, message: 'El nombre del producto es requerido' } })}
-							key="nameclient"
-							type="text"
-							label={<span className="custom-label">Nombre</span>}
-							labelPlacement="outside"
-							placeholder={"Escriba el nombre del producto"}
-							size="md"
-						/>
-						{errors.nameclient && <span>{errors.nameclient.message}</span>}
+						<div className="flex flex-col">
+							<InputText ref={(el) => { refs.current.idCardClient = el; }}
+								{...register("idCardClient", { required: { value: true, message: mensaje } })}
+								key="idCardClient"
+								type="text"
+								label={<span className="custom-label">Cédula</span>}
+								labelPlacement="outside"
+								placeholder={"Escriba el nombre del producto"}
+								size="md"
+							/>
+							{errors.idCardClient && <span>{errors.idCardClient.message}</span>}
+						</div>
+						<div className="flex flex-col">
+							<InputText ref={(el) => { refs.current.nameclient = el; }}
+								{...register("nameclient", { required: { value: true, message: mensaje } })}
+								key="nameclient"
+								type="text"
+								label={<span className="custom-label">Nombre</span>}
+								labelPlacement="outside"
+								placeholder={"Escriba el nombre del producto"}
+								size="md"
+							/>
+							{errors.nameclient && <span>{errors.nameclient.message}</span>}
+						</div>
 					</div>
 					<Spacer y={4} />
 					<div className="gap-4" style={{ display: "grid", gridTemplateColumns: "2fr 2fr" }}>
-						<InputText ref={(el) => { refs.current.clientAddress = el; }}
-							{...register("clientAddress", { required: { value: true, message: 'El nombre del producto es requerido' } })}
-							key="clientAddress"
-							type="text"
-							label={<span className="custom-label">Dirección</span>}
-							labelPlacement="outside"
-							placeholder={"Escriba el nombre del producto"}
-							size="md"
-						/>
-						{errors.clientAddress && <span>{errors.clientAddress.message}</span>}
-
-						<InputText ref={(el) => { refs.current.clientPhone = el; }}
-							{...register("clientPhone", { required: { value: true, message: 'El nombre del producto es requerido' } })}
-							key="clientPhone"
-							type="text"
-							label={<span className="custom-label">Celular</span>}
-							labelPlacement="outside"
-							placeholder={"Escriba el nombre del producto"}
-							size="md"
-						/>
-						{errors.clientPhone && <span>{errors.clientPhone.message}</span>}
+						<div className="flex flex-col">
+							<InputText ref={(el) => { refs.current.clientAddress = el; }}
+								{...register("clientAddress", { required: { value: true, message: mensaje } })}
+								key="clientAddress"
+								type="text"
+								label={<span className="custom-label">Dirección</span>}
+								labelPlacement="outside"
+								placeholder={"Escriba el nombre del producto"}
+								size="md"
+							/>
+							{errors.clientAddress && <span>{errors.clientAddress.message}</span>}
+						</div>
+						<div className="flex flex-col">
+							<InputText ref={(el) => { refs.current.clientPhone = el; }}
+								{...register("clientPhone", { required: { value: true, message: mensaje } })}
+								key="clientPhone"
+								type="text"
+								label={<span className="custom-label">Celular</span>}
+								labelPlacement="outside"
+								placeholder={"Escriba el nombre del producto"}
+								size="md"
+							/>
+							{errors.clientPhone && <span>{errors.clientPhone.message}</span>}
+						</div>
 					</div>
 					<Spacer y={4} />
-					</Acordeon>
-			
-			<Spacer y={4} />
-			<Acordeon titulo={'Lista de productos'}>
+				</Acordeon>
 
-			</Acordeon>
-	
-			<Spacer y={5} />
-			<Card className="" style={{ width: "90%", margin: "0 auto" }}>
-  			<CardHeader className="flex gap-3">
-    			<p style={{ fontFamily: "Roboto, sans-serif", fontSize: "20px", fontWeight: "bold" }}>Lista de productos</p>
-  			</CardHeader>
-  			<Divider />
-  			<CardBody className="cardProduct">
-    			{state.selectedProducts.map((product, index) => (
-      				<div key={index}>
-						<img className="img" src={product.img} alt={product.producto} />
-        				<p className="p1">{product.producto}</p>
-       				 	<p className="p2">{product.precio}</p>
-      				</div>
-    			))}
-  			</CardBody>
-			  <Spacer y={4} />
-			</Card>
+				<Spacer y={4} />
+				<Acordeon titulo={'Lista de productos'}>
+					<Card className="" style={{ width: "100%", margin: "0 auto" }}>
 
+						<CardBody className="cardProduct">
+							{state.selectedProducts.map((product, index) => (
+								<div key={index}>
+									<img className="img" src={product.img} alt={product.producto} />
+									<p className="p1">{product.producto}</p>
+									<p className="p2">{product.precio}</p>
+								</div>
+							))}
+						</CardBody>
+						<Spacer y={4} />
+					</Card>
+				</Acordeon>
+				<Spacer y={5} />
 
-			<div style={{ width: "100%", height: "100px", padding: "10px", display: "flex", alignItems: "center", gap: "30px", position: "absolute", backgroundColor: "#454F96", borderRadius: "30px" }}>
-
-				<BotonEnviar
-					text={"Comprar"}
-					type={"submit"}
-				/>
-
-			</div>
+				<div style={{ width: "100%", height: "100px", padding: "10px", display: "flex", alignItems: "center", gap: "30px", position: "absolute", backgroundColor: "#454F96", borderRadius: "30px" }}>
+					<BotonEnviar text={"Comprar"} type={"submit"} />
+				</div>
+			</form>
 			<Spacer y={4} />
 			<Footer />
 		</div>
