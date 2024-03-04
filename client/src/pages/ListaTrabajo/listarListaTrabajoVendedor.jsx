c45dad83651303d0814c609e9efee988c627dd02
sarac721 <123259230+sarac721@users.noreply.github.com>
2024-03-03 22:04:59 -0500

    Se hizo tarjeta para lista de trabajo Trabajador

--- client/src/pages/ListaTrabajo/listarListaTrabajoVendedor.jsx
+++ client/src/pages/ListaTrabajo/listarListaTrabajoVendedor.jsx
@@ -4,6 +4,8 @@ import Flotante from '../../components/Botones/BotonFlotante/Flotante';
 
 import Navigate, { Notificacion, Retroceder, Titulo } from "../../components/UI/navbar/navbar";
 import Footer from "../../components/UI/Footer/Footer";
+import '../ListaTrabajo/ListaTrabajo.css'
+import CardPerfil, { IconoCard, Texto1Card, Texto2Card } from "../../components/UI/perfil/cardInfo";
 
 function ListarListaTrabajoVendedor() {
 	return (
@@ -29,6 +31,52 @@ function ListarListaTrabajoVendedor() {
 					<Retroceder />
 					<Titulo espacio="center" titulo="Lista de trabajo" />
 				</Navigate>
+				<div style={{marginBottom:"30px"}}>
+				</div>
+                 <div className="cont1ListaT">
+                 <CardPerfil
+                 className="card1ListaT"
+                 width={"250px"}
+                 height={"150px"}>
+					   <div className="cont2ListaT">
+
+                        <div className="contTexto1">
+<Texto1Card
+texto={"Nombre de la lista"}
+/>
+	
+                        </div>
+                        
+                        <div
+                        className="card2ListaT">
+                        <div className="contTexto2">
+                        <Texto2Card
+                        
+                        texto2={"Nombre trabajador"}
+                        />
+                        </div>	
+						<div className="contIcono">
+                        <IconoCard
+                        icon={"bi:currency-dollar"} 
+                        width={"22px"}
+                        height={"22px"}/>
+                        </div>
+                        <div className="contIcono">
+                        <IconoCard
+                        icon={"bi:check-circle-fill"}
+                        width={"22px"}
+                        height={"22px"}/>
+                        </div>
+						
+                        
+                        </div>
+                        </div>
+                        </CardPerfil>
+                        
+                        </div>
+
+
+
 
 				<Footer />
 			</div>
