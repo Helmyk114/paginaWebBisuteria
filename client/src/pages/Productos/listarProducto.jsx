70fc9c3574301604a5a79ed01306b30affdad4d6
sarac721 <123259230+sarac721@users.noreply.github.com>
2024-03-14 21:31:02 -0500

    Ajustes css tarjeta crear pedido

--- client/src/pages/Productos/listarProducto.jsx
+++ client/src/pages/Productos/listarProducto.jsx
@@ -1,7 +1,5 @@
 import React, { useState, useEffect } from "react";
 
-import { cambiarEstadoInformacionApi, listarInformacionConParametroApi } from "../../api/productos";
-import Swal from "sweetalert2";
 import "../Productos/listarProductoVendedor.css"
 
 import { Spacer, Tooltip } from "@nextui-org/react";
@@ -158,6 +156,7 @@ function ListarProducto() {
 										<CardPerfil
 											className1={"cardListaPro"}
 											className2={"cardListaProGap"}
+											
 											key={datos.idProduct}>
 											<div className="inactivoCardListaPro">
 												<Avatares
