7ffa03e3dd9bc5bbe408644d197a99b5573691b2
sarac721 <123259230+sarac721@users.noreply.github.com>
2024-03-22 20:05:54 -0500

    Ajustes css en tarjetas

--- client/src/pages/ListaTrabajo/listarListaTrabajoTrabajador.jsx
+++ client/src/pages/ListaTrabajo/listarListaTrabajoTrabajador.jsx
@@ -16,14 +16,11 @@ function ListarListaTrabajoVendedor() {
 			<div style={{ marginBottom: "30px" }}>
 			</div>
 			<div className="cont1ListaT">
-			
+
 				<CardPerfil
-					className="card1ListaT"> 
-					
+					className="card1ListaT">
 
 					<div className="cont2ListaT">
-					
-
 						<div className="contTexto1">
 							<Texto1Card
 								texto={"Nombre de la lista"}
@@ -34,113 +31,169 @@ function ListarListaTrabajoVendedor() {
 							/>
 
 						</div>
-						
-							
-
-					
 
 						<div
 							className="card2ListaT">
-								
+
 							<div className="contTexto2">
 								<Texto2Card
-
 									texto2={"Ver"}
+									fontWeight={"400"}
 								/>
 							</div>
 							<div className="contIcono">
-							
+
 								<IconoCard
 									icon={"akar-icons:arrow-right"}
-									width={"42px"}
-									height={"42px"} />
-							
-							
+									className="iconoFlecha"
+								/>
 
 							</div>
-						
+
 						</div>
 					</div>
-					
+
 				</CardPerfil>
-				
+
 
 			</div>
 
 			<div>
 				<Acordeon titulo={"Lista "}>
-				<div className="cont1ListaT">
-			
-			<CardPerfil
-				className="card1ListaT"> 
-				<div className="contenedor1ListaT" style={{display:"flex"}}>
+					<div className="cont1ListaT">
 
-					
-					 <div className="contIconoListaT"style={{backgroundColor:"#6977E4 ", borderRadius:"10px", marginRight:"8px", width:"60%", display:"column", padding:"30px"}} >
-					 <div className="contIcono">
-						
-						<IconoCard
-							icon={"icon-park-solid:check-one"}
-							width={"42px"}
-							height={"42px"} 
-							color={"#ffff"}
-					/>
-					
+						<CardPerfil
+							className="card1ListaT">
+							<div className="contenedor1ListaT">
+
+
+								<div className="contIconoListaT" >
+									<div className="contIconoCheck" style={{ justifyContent: "center" }}>
+
+										<IconoCard
+											icon={"icon-park-solid:check-one"}
+											color={"#ffff"}
+										/>
+
+									</div >
+									<Texto1Card
+
+										texto={"Finalizdo"}
+										color={"#ffff"}
+										fontWeight={"400"}
+										className="contTexto1" />
+								</div>
+
+								<div className="cont2ListaT">
+
+
+									<div className="contTexto1">
+										<Texto1Card
+											texto={"Nombre de la lista"}
+										/>
+										<Texto1Card
+											texto={"Codigo"}
+											fontWeight={"200"}
+										/>
+
+									</div>
+
+									<div
+										className="card2ListaT">
+
+										<div className="contTexto2">
+											<Texto2Card
+												texto2={"Pago total: 30.000"}
+												fontWeight={"200"}
+											/>
+										</div>
+										<div className="contIcono">
+
+											<IconoCard
+												icon={"akar-icons:arrow-right"}
+												className="iconoFlecha"
+											/>
+
+										</div>
+
+									</div>
+								</div>
+							</div>
+
+						</CardPerfil>
+
+
 					</div>
-					<Texto1Card
-					
-					texto={"Finalizdo"}
-					color={"#ffff"}
-					fontWeight={"300"}/>
+
+				</Acordeon>
+			</div>
+			<div>
+				<Acordeon titulo={"Lista "}>
+					<div className="cont1ListaT">
+
+						<CardPerfil
+							className="card1ListaT">
+							<div className="contenedor1ListaT">
+
+
+								<div className="contIconoListaT" >
+									<div className="contIconoCheck" style={{ justifyContent: "center" }}>
+
+										<IconoCard
+											icon={"icon-park-solid:check-one"}
+											color={"#ffff"}
+										/>
+
+									</div >
+									<Texto1Card
+
+										texto={"Finalizdo"}
+										color={"#ffff"}
+										fontWeight={"400"}
+										className="contTexto1" />
+								</div>
+
+								<div className="cont2ListaT">
+
+
+									<div className="contTexto1">
+										<Texto1Card
+											texto={"Nombre de la lista"}
+										/>
+										<Texto1Card
+											texto={"Codigo"}
+											fontWeight={"200"}
+										/>
+
+									</div>
+
+									<div
+										className="card2ListaT">
+
+										<div className="contTexto2">
+											<Texto2Card
+												texto2={"Pago total: 30.000"}
+												fontWeight={"200"}
+											/>
+										</div>
+										<div className="contIcono">
+
+											<IconoCard
+												icon={"akar-icons:arrow-right"}
+												className="iconoFlecha"
+											/>
+
+										</div>
+
+									</div>
+								</div>
+							</div>
+
+						</CardPerfil>
+
+
 					</div>
 
-				<div className="cont2ListaT">
-				
-
-					<div className="contTexto1">
-						<Texto1Card
-							texto={"Nombre de la lista"}
-						/>
-						<Texto1Card
-							texto={"Codigo"}
-							fontWeight={"200"}
-						/>
-
-					</div>
-					
-						
-
-				
-
-					<div
-						className="card2ListaT">
-							
-						<div className="contTexto2">
-							<Texto2Card
-
-								texto2={"Ver"}
-							/>
-						</div>
-						<div className="contIcono">
-						
-							<IconoCard
-								icon={"akar-icons:arrow-right"}
-								width={"42px"}
-								height={"42px"} />
-						
-						
-
-						</div>
-					
-					</div>
-				</div>
-				</div>
-				
-			</CardPerfil>
-			
-
-		</div>
-					
 				</Acordeon>
 			</div>
 
