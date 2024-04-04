07811127db8ad9c0d4ce7b5e994d123223333acf
sarac721 <123259230+sarac721@users.noreply.github.com>
2024-04-04 10:46:00 -0500

    ajaustes css notificaciones

--- client/src/pages/Notificaciones/notificacionesArtesano.jsx
+++ client/src/pages/Notificaciones/notificacionesArtesano.jsx
@@ -1,12 +1,15 @@
 import React, { useEffect, useState } from 'react';
 
-import { Spacer } from '@nextui-org/react';
+import { Spacer, Tooltip } from '@nextui-org/react';
 import NavigateTRJ, { Retroceder, Titulo } from "../../components/UI/navbar/navbarTrabajador";
 import Loader from '../../components/UI/cargando/loader';
 import Footer from '../../components/UI/Footer/Footer';
+import '../Notificaciones/notificaciones.css'
 
 import { listarInformacionConParametroApi } from '../../api/axiosServices';
 import { decodificarToken, obtenerToken } from '../../utils/token';
+import CardPerfil, { IconoCard, Texto1Card, Texto2Card } from '../../components/UI/perfil/cardInfo';
+import DeleteIcon from '../../components/UI/iconos/Eliminar';
 
 function NotificacionesArtesano() {
   const [notificacion, setNotificacion] = useState([]);
@@ -39,13 +42,58 @@ function NotificacionesArtesano() {
 			{cargando ? (
 				<Loader />
 			) : (
-				<div style={{ display: "flex", flexWrap: "wrap", gap: "5px", justifyContent: "center", }}>
+				<div style={{ display: "flex", flexWrap: "wrap", gap: "10px", justifyContent: "center", }}>
 					{notificacion && notificacion.length > 0 ? (notificacion.map((noti) => (
 							<div key={noti.idNotification}>
-								<h1>{noti.title}</h1>
-                <h3>{noti.message}</h3>
-                <p>fecha: {noti.fecha}</p>
-                <p>hora: {noti.hora}</p>
+								<CardPerfil className1={"cardNotificaciones"}
+								className2={"cardNotificacionesGap"}>
+									
+								<div className='contNotificaciones'>
+									
+									<div className='cont1Notificaciones'>
+									<div className='contIconoN'>
+										<IconoCard
+											icon="mingcute:notification-line"
+											width={"25px"} />
+									</div>
+                                        <div className='contTxtPrincipal'>
+										<Texto1Card texto={noti.title} />
+										<Texto2Card texto2={noti.message} />
+										</div>
+										<div className='iconoEliminarN'>
+											<Tooltip content="Eliminar ">
+												<span className="text-lg text-danger cursor-pointer active:opacity-50">
+													<DeleteIcon />
+												</span>
+											</Tooltip>
+										</div>
+									</div>
+									
+									<Spacer y={2} />
+									<div className='cont2Notificaciones'>
+										<div className='contFecha'>
+											<Texto2Card texto2={"Fecha: "}
+												fontSize={"13px"} />
+											<Spacer x={1} />
+											<Texto2Card texto2={noti.fecha}
+												fontSize={"13px"} />
+										</div>
+
+										<div className='contHora'>
+											<Texto2Card texto2={"Hora: "}
+												fontSize={"13px"} />
+											<Spacer x={1} />
+											<Texto2Card texto2={noti.hora}
+												fontSize={"13px"} />
+										</div>
+										<Spacer y={4} />
+										
+									</div>
+									
+								</div>
+							</CardPerfil>
+
+							<Spacer y={2} />
 							</div>
 						))
 					) : (
--- client/src/pages/Notificaciones/notificacionesVendedor.jsx
+++ client/src/pages/Notificaciones/notificacionesVendedor.jsx
@@ -1,12 +1,15 @@
 import React, { useState, useEffect } from 'react';
 
-import { Spacer } from '@nextui-org/react';
+import { Spacer, Tooltip } from '@nextui-org/react';
 import NavigateVEN, { Retroceder, Titulo } from "../../components/UI/navbar/navbarVendedor";
 import Loader from '../../components/UI/cargando/loader';
 import Footer from '../../components/UI/Footer/Footer';
 
 import { listarInformacionConParametroApi } from '../../api/axiosServices';
 import { decodificarToken, obtenerToken } from '../../utils/token';
+import '../Notificaciones/notificaciones.css'
+import CardPerfil, { IconoCard, Texto1Card, Texto2Card } from '../../components/UI/perfil/cardInfo';
+import DeleteIcon from '../../components/UI/iconos/Eliminar';
 
 function NotificacionesVendedor() {
 	const [notificacion, setNotificacion] = useState([]);
@@ -41,10 +44,55 @@ function NotificacionesVendedor() {
 				<div style={{ display: "flex", flexWrap: "wrap", gap: "5px", justifyContent: "center", }}>
 					{notificacion && notificacion.length > 0 ? (notificacion.map((noti) => (
 							<div key={noti.idNotification}>
-								<h1>{noti.title}</h1>
-                <h3>{noti.message}</h3>
-                <p>fecha: {noti.fecha}</p>
-                <p>hora: {noti.hora}</p>
+								<CardPerfil className1={"cardNotificaciones"}
+								className2={"cardNotificacionesGap"}>
+									
+								<div className='contNotificaciones'>
+									
+									<div className='cont1Notificaciones'>
+									<div className='contIconoN'>
+										<IconoCard
+											icon="mingcute:notification-line"
+											width={"25px"} />
+									</div>
+                                        <div className='contTxtPrincipal'>
+										<Texto1Card texto={noti.title} />
+										<Texto2Card texto2={noti.message} />
+										</div>
+										<div className='iconoEliminarN'>
+											<Tooltip content="Eliminar ">
+												<span className="text-lg text-danger cursor-pointer active:opacity-50">
+													<DeleteIcon />
+												</span>
+											</Tooltip>
+										</div>
+									</div>
+									
+									<Spacer y={2} />
+									<div className='cont2Notificaciones'>
+										<div className='contFecha'>
+											<Texto2Card texto2={"Fecha: "}
+												fontSize={"13px"} />
+											<Spacer x={1} />
+											<Texto2Card texto2={noti.fecha}
+												fontSize={"13px"} />
+										</div>
+
+										<div className='contHora'>
+											<Texto2Card texto2={"Hora: "}
+												fontSize={"13px"} />
+											<Spacer x={1} />
+											<Texto2Card texto2={noti.hora}
+												fontSize={"13px"} />
+										</div>
+										<Spacer y={4} />
+										
+									</div>
+									
+								</div>
+							</CardPerfil>
+
+							<Spacer y={2} />
 							</div>
 						))
 					) : (
